7e3508439fad70273587c2e0821ea63e5fe61692
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-06-19 12:15:20 +0200

    Initial form config

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,27 @@
 import React, {Component} from 'react';
 
 class Form extends Component {
+
+  handleSubmit = event =>
+    event.preventDefault()
+
   render() {
     return (
-      <div>
-        test
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          name="startingPoint"
+          aria-label="Starting point"
+          placeholder="Starting point"
+        />
+        <input
+          type="text"
+          name="destination"
+          aria-label="Destination"
+          placeholder="Destination"
+        />
+        <button>Submit</button>
+      </form>
     );
   }
 }
