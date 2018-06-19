73f355213c80ca0e1e30164caffe8cc50924a06e
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-06-19 13:21:00 +0200

    Fetching directions from google API

--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,16 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import GoogleMapsLoader from 'google-maps'
+
+const apiKey = 'AIzaSyC_1ShOt4e8e2kogxZ_rR1VEzHdUBnFjMo'
+GoogleMapsLoader.KEY = apiKey
 
 class Form extends Component {
 
   state = {
     startingPoint: '',
-    destination: ''
+    destination: '',
+    fetching: false,
+    error: null
   }
 
   handleChange = event =>
@@ -12,30 +18,66 @@ class Form extends Component {
       [event.target.name]: event.target.value
     })
 
-  handleSubmit = event =>
+  handleSubmit = event => {
     event.preventDefault()
+
+    this.setState({
+      fetching: true,
+      error: null
+    })
+
+    const startingPoint = this.state.startingPoint
+    const destination = this.state.destination
+    const fetchingFinished = () =>
+      this.setState({
+        fetching: false,
+        error: null
+      })
+
+    GoogleMapsLoader.load(function (google) {
+      const request = {
+        origin: startingPoint,
+        destination: destination,
+        travelMode: 'DRIVING'
+      }
+      const directionsService = new google.maps.DirectionsService()
+      // const directionsDisplay = new google.maps.DirectionsRenderer()
+      // directionsDisplay.setPanel(document.getElementById('directionsPanel'))
+      directionsService.route(request, function (result, status) {
+        if (status === 'OK') {
+          console.log(result)
+          fetchingFinished()
+          // directionsDisplay.setDirections(result)
+        }
+      })
+    })
+  }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          name="startingPoint"
-          aria-label="Starting point"
-          placeholder="Starting point"
-          value={this.state.startingPoint}
-          onChange={this.handleChange}
-        />
-        <input
-          type="text"
-          name="destination"
-          aria-label="Destination"
-          placeholder="Destination"
-          value={this.state.destination}
-          onChange={this.handleChange}
-        />
-        <button>Submit</button>
-      </form>
+      <Fragment>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="startingPoint"
+            aria-label="Starting point"
+            placeholder="Starting point"
+            value={this.state.startingPoint}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="destination"
+            aria-label="Destination"
+            placeholder="Destination"
+            value={this.state.destination}
+            onChange={this.handleChange}
+          />
+          <button>Submit</button>
+        </form>
+        <div id="directionsPanel">
+        </div>
+      </Fragment>
     );
   }
 }
