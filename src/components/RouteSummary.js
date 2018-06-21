68f1b9cf3d8b8adcb136834c48f1af3d5e286fb1
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-06-21 18:22:24 +0200

    WIP: RouteSummary table with directions

--- src/components/RouteSummary.js
+++ src/components/RouteSummary.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, {Component, Fragment} from 'react'
 import {withForm} from '../contexts/Form'
 import './RouteSummary.css'
 
@@ -11,8 +11,11 @@ class RouteSummary extends Component {
   }
 
   render() {
+    const fetchedResultLegs = this.props.fetchedResult && this.props.fetchedResult.routes[0].legs
+    const fetchedResultAltLegs = this.props.fetchedResultAlt && this.props.fetchedResultAlt.routes[0].legs
+    console.log('fetchedResultLegs', fetchedResultLegs, 'fetchedResultAltLegs', fetchedResultAltLegs)
     return (
-      <div>
+      <Fragment>
         {
           this.props.totalDistance &&
           this.props.totalDuration &&
@@ -26,15 +29,53 @@ class RouteSummary extends Component {
               {
                 (this.props.optimization === 'shortest' && this.props.totalDistance <= this.props.totalDistanceAlt) ||
                 (this.props.optimization === 'fastest' && this.props.totalDuration <= this.props.totalDurationAlt) ?
+                  <div>
                   <p>Total distance: {this.props.totalDistance} kilometers. Total
-                    duration: {this.props.totalDuration} minutes.</p> :
+                    duration: {this.props.totalDuration} minutes.</p>
+                    <div className="route-summary__table">
+                      <div className="route-summary__table-row route-summary__table-heading">
+                        <div className="route-summary__table-cell route-summary__table-cell-first">
+                          Waypoints
+                        </div>
+                        <div className="route-summary__table-cell">
+                          Distance
+                        </div>
+                        <div className="route-summary__table-cell">
+                          Duration
+                        </div>
+                      </div>
+                      <div className="route-summary__table-row">
+                        <div className="route-summary__table-cell route-summary__table-cell-first">
+                          Bukowa 2
+                        </div>
+                        <div className="route-summary__table-cell">
+
+                        </div>
+                        <div className="route-summary__table-cell">
+
+                        </div>
+                      </div>
+                      <div className="route-summary__table-row">
+                        <div className="route-summary__table-cell route-summary__table-cell-first">
+                          Leśna 15
+                        </div>
+                        <div className="route-summary__table-cell">
+                          17km
+                        </div>
+                        <div className="route-summary__table-cell">
+                          25min
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  :
                   <p>Total distance: {this.props.totalDistanceAlt} kilometers. Total
                     duration: {this.props.totalDurationAlt} minutes.</p>
               }
             </div>
           </div>
         }
-      </div>
+      </Fragment>
     )
   }
 }
